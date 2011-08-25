4780d6a1bf1bc1184e438f1bbe12a97134d2bf02
Michael Haren <mharen@gmail.com>
2011-08-24 21:55:06 -0400

    updated example with input-scoped detection; added triple-equals because triple ftw?

--- jquery.keysequencedetector.js
+++ jquery.keysequencedetector.js
@@ -27,17 +27,16 @@
 	// sequence: a string of characters to watch for
 	// action: a function to execute when the sequence is detected
 	// options: a hash of additional options
-
 	jQuery.fn.keySequenceDetector = function(sequence, action, options) {
 		var settings = $.extend({}, $.fn.keySequenceDetector.defaultOptions, options);
 		
 		return this.each(function() {
 			var i = 0;
 			$(this).keypress(function(e) {
-				var key =  String.fromCharCode(e.which);
+				var key = String.fromCharCode(e.which);
 				if (sequence[i] === key) {
 				++i;
-					if (sequence.length == i) {
+					if (sequence.length === i) {
 						i = 0;
 						action();
 					}
